24aea90c3230087bb0e0681cf4f8f1768bdf94bd
Egor <eg.terentjev2016@yandex.ru>
2023-10-17 16:09:12 +0200

    Auto push

--- Source Files/main.cpp
+++ Source Files/main.cpp
@@ -12,7 +12,7 @@ int main() {
 }
 
 void main_window() {
-	Journal journal1[N]; //��������� ����������� �� ���������
+	Journal journal1[N]; 
 	for (int i = 0; i < N; i++)
 		journal1[i].print();
 	cout << endl;
@@ -21,16 +21,17 @@ void main_window() {
 	journal1[2].edit_journal("testname3", 5, 465732);
 	journal1[3].edit_journal("testname4", 3, 149507);
 	journal1[4].edit_journal("testname5", 2, 157202);
+	
 	for (int i = 0; i < N; i++)
 		journal1[i].print();
 	cout << endl;
-	Journal journal2("Constructor with param", 4, 354362); //���� ������������ � �����������
+	Journal journal2("Constructor with param", 4, 354362); 
 	journal2.print();
 	cout << endl;
-	Journal copy = journal1[3]; //���� ������������ �����������
+	Journal copy = journal1[3]; 
 	copy.print();
 	cout << endl;
-	journal1[0].sum_journal(journal1[2]); //���� �����
+	journal1[0].sum_journal(journal1[2]); 
 	journal1[0].print();
 
 	
